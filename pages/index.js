53217f9f69bc2969e0420b39ff0476eefdd00423
khai96_ <hvksmr1996@gmail.com>
2018-01-20 14:04:02 +0700

    Complete

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+
+export default () => <div>
+  <AppBar>
+    <Toolbar>
+      <Typography type='title' color='inherit'>
+        Hello, World!!
+      </Typography>
+    </Toolbar>
+  </AppBar>
+</div>
